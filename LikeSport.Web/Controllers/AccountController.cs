4153616c1e6c15cdb9f774c6d0096a063b03e650
Đỗ Văn Tú <tieutuvdanh@gmail.com>
2017-06-27 22:52:06 +0700

    no message

---
+++ LikeSport.Web/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LikeSport.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        //public ActionResult Profile()
+        //{
+        //    return View();
+        //}
+    }
+}
